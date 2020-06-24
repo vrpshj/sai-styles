9caa495f15a70cae081309bf733399f9f8056116
Ram Prakash Vellingiri <vrpshj@yahoo.com>
2020-06-24 21:58:43 +1000

    created a shop page

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './menu-item.styles.scss';
+import {withRouter} from 'react-router-dom';
 
-
-const MenuItem =({title,imageUrl,size,linkUrl}) =>
+const MenuItem =({title,imageUrl,size,history,linkUrl,match}) =>
 (
-    <div  className={`${size} menu-item`}>
+    <div  className={`${size} menu-item`} onClick={()=>history.push(`${match.url}${linkUrl}`)}>
         <div className='background-image' style={{backgroundImage:`url(${imageUrl})`}}></div>
     <div className='content'>
         <h1 className='title'>{title.toUpperCase()}</h1>
@@ -13,4 +13,4 @@ const MenuItem =({title,imageUrl,size,linkUrl}) =>
 </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
   BrowserRouter
 } from "react-router-dom";
 
